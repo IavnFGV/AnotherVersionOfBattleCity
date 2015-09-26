d25cb650d58867feb8979db9c00b941dfb200cee
IavnFGV <ivandrozda@rambler.ru>
2015-09-27 00:14:23 +0300

    iteration

--- src/main/java/com/drozda/battlecity/GameUnit.java
+++ src/main/java/com/drozda/battlecity/GameUnit.java
@@ -5,23 +5,68 @@ import javafx.beans.property.*;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
+import javafx.geometry.BoundingBox;
+import javafx.geometry.Bounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+
+import static com.drozda.battlecity.StaticServices.ONE_SECOND;
+import static java.util.Arrays.asList;
 
 /**
  * Created by GFH on 26.09.2015.
  */
 public class GameUnit extends Observable {
     private static final Logger log = LoggerFactory.getLogger(GameUnit.class);
+    protected static Map<State, Long> defaultTimeInState = new EnumMap<>(State.class);
 
+    static {
+        defaultTimeInState.put(State.CREATING, 2 * ONE_SECOND);
+        defaultTimeInState.put(State.ACTIVE, 0L);
+        defaultTimeInState.put(State.EXPLODING, ONE_SECOND / 6);
+        defaultTimeInState.put(State.DEAD, 0L);
+
+    }
+
+    protected List<State> defaultStateFlow = asList(State.CREATING, State.ACTIVE, State.EXPLODING, State.DEAD);
+    protected ObjectProperty<Bounds> bounds = new SimpleObjectProperty<>(new BoundingBox(0, 0, 0, 0));
     private Map<State, Long> timeInState = new EnumMap<>(State.class);
     private ObjectProperty<State> currentState = new SimpleObjectProperty<>();
     private BooleanProperty pause = new SimpleBooleanProperty();
     private LongProperty heartBeats = new SimpleLongProperty();
     private ListProperty<BonusUnit.BonusType> bonusList = new SimpleListProperty<>();
-    private List<State> stateFlow = new LinkedList();
+    private List<State> stateFlow = new LinkedList(); //TODO maybe we can use LinkedHashMap??
+
+    public GameUnit(double minX, double minY, double width, double height, List<State> stateFlow, Map<State, Long>
+            timeInState) {
+        this.setBounds(new BoundingBox(minX, minY, width, height));
+        if (stateFlow == null) {
+            this.stateFlow.addAll(defaultStateFlow);
+            this.timeInState.putAll(defaultTimeInState);
+        } else {
+            this.timeInState.putAll(timeInState);
+            for (State state : stateFlow) {
+                this.timeInState.putIfAbsent(state, defaultTimeInState.get(state));
+            }
+        }
+
+
+    }
+
+    public Bounds getBounds() {
+        return bounds.get();
+    }
+
+    public void setBounds(Bounds bounds) {
+        this.bounds.set(bounds);
+    }
+    //  protected Map<State,Long> timeInStateMap = new LinkedHashMap<>();
+
+    public ObjectProperty<Bounds> boundsProperty() {
+        return bounds;
+    }
 
     public State getCurrentState() {
         return currentState.get();
@@ -59,6 +104,9 @@ public class GameUnit extends Observable {
         return bonusList;
     }
 
+    protected Long getTimeInState(State state) {
+        return timeInState.get(state);
+    }
 
     public enum State {
         CREATING,
@@ -108,28 +156,9 @@ public class GameUnit extends Observable {
             int curIndex = stateFlow.indexOf(currentState);
             if (curIndex < stateFlow.size() - 1) {
                 setCurrentState(stateFlow.get(curIndex + 1));
+                setChanged();
             }
         }
     }
 
-    protected class BasicHeartBeatListener extends HeartBeatListener {
-
-        public void perform(long deltaTime) {
-            changeBasicState(deltaTime);
-        }
-
-        public void changeBasicState(Long deltaTime) {
-            if (getTimeInState(getBasicState()) > 0) { // so we can tick time
-                setLeftTimeInBasicState(getLeftTimeInBasicState() - deltaTime);
-                if ((getLeftTimeInBasicState()) <= 0) {
-                    if (getBasicState() == ua.drozda.battlecity.core.GameUnit.BasicState.CREATING) {
-                        setBasicState(ua.drozda.battlecity.core.GameUnit.BasicState.ACTIVE);
-                    } else {
-                        setBasicState(BasicState.DEAD);
-                    }
-                    setChanged();
-                }
-            }
-        }
-    }
 }
